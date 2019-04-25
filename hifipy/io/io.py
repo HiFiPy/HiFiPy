997baa12d4e962b349e16830d3379dc48b29bb36
Nick Murphy <namurphy@cfa.harvard.edu>
2019-04-25 15:39:45 -0400

    Use @property for attributes in hifi_class

--- hifipy/io/io.py
+++ hifipy/io/io.py
@@ -1,8 +1,8 @@
 """
 Routines to read in postprocessed HiFi simulation results.
 
-Notes 
------ 
+Notes
+-----
 The directory specified in postpath should include contents such as
 grid_00000.h5, post_00000.h5, post_00000.xmf, ...
 
@@ -21,7 +21,7 @@ To see the keys that are contained in each of the HDF5 files, use
     file_list[0].keys()
 
 The first key will typically be "U01".  To get a slice of the "U01" in
-the third output file, use 
+the third output file, use
 
     file_list[2]["U01"][:,0]
 """
@@ -34,15 +34,15 @@ import numpy as np
 import glob
 
 
-def read_grid(postpath='.'):   
+def read_grid(postpath='.'):
     """
-    Reads in grid information from a HiFi simulation.  
+    Reads in grid information from a HiFi simulation.
 
     Inputs
     ------
     The sole input, postpath, must be a directory that contains the
-    output files from a HiFi simulation, including a grid file such as 
-    grid_00000.h5.  
+    output files from a HiFi simulation, including a grid file such as
+    grid_00000.h5.
 
     Sample usage
     ------------
@@ -50,9 +50,9 @@ def read_grid(postpath='.'):
     """
     assert isdir(expanduser(postpath)), \
         "read_grid: The input must be a directory."
-    
-    files_grid = glob.glob(postpath+'/grid*.h5') 
-    
+
+    files_grid = glob.glob(postpath+'/grid*.h5')
+
     assert len(files_grid) > 0, \
         "read_grid: Grid file not found. Are you using the correct directory when calling read_grid?"
 
@@ -64,7 +64,7 @@ def read_grid(postpath='.'):
     x_2D, y_2D = np.array(grid['U01']), np.array(grid['U02'])
     x_1D, y_1D = np.array(x_2D[0,:]), np.array(y_2D[:,0])
 
-    return x_2D, y_2D, x_1D, y_1D    
+    return x_2D, y_2D, x_1D, y_1D
 
 
 def find_files_and_time(postpath='.'):
@@ -90,13 +90,13 @@ def find_files_and_time(postpath='.'):
     nfilemax = len(files_h5)
 
     # Extract the time from the xmf files
-    
+
     time = np.zeros(nfilemax)
 
     try:
         for i in range(0,nfilemax):
             file_xmf = open(files_xmf[i],'r')
-            lines = file_xmf.readlines()                            
+            lines = file_xmf.readlines()
             time[i] = np.double(lines[5][18:30])
     except:
         print("find_files_and_time: Problem with reading in the time from the .xmf files.")
@@ -108,7 +108,7 @@ def find_files_and_time(postpath='.'):
 def read_directory(postpath='.', verbose=True):
     """
     Creates a list of h5py File objects that correspond to each
-    postprocessed HiFi simulation output file.  
+    postprocessed HiFi simulation output file.
 
     Sample usage
     ------------
@@ -137,7 +137,7 @@ def read_directory(postpath='.', verbose=True):
 
     assert len(file_list) > 0, \
         "HDF5 files not found. Are you using the correct directory when calling read_directory?"
-                
+
     return file_list, time
 
 
@@ -157,22 +157,22 @@ class hifi_class(object):
     To access a given axis or HiFi variable, use any of
 
         data_set.x
-        data_set.y 
+        data_set.y
         data_set.time
 
-        data_set.ni 
-        data_set.Az 
-        data_set.Bz 
+        data_set.ni
+        data_set.Az
+        data_set.Bz
         data_set.Vix
         data_set.Viy
         data_set.Viz
-        data_set.Jz 
-        data_set.pi 
-        data_set.nn 
+        data_set.Jz
+        data_set.pi
+        data_set.nn
         data_set.Vnx
         data_set.Vny
         data_set.Vnz
-        data_set.pn 
+        data_set.pn
 
     To access the simulation ID, use
 
@@ -180,24 +180,26 @@ class hifi_class(object):
 
     The default simulation ID is None
     """
-    
+
     def __init__(self, postpath, simID=None):
         x, y, xx, yy = read_grid(postpath)
         file_list, time = read_directory(postpath)
-        
+
         #axes for plotting
-        self.x = xx
-        self.y = yy
-        self.time = time
-        
+        self._x = xx
+        self._y = yy
+        self._time = time
+
+        self._file_list = file_list
+
         if simID is not None:
-            self.name = simID
+            self._name = simID
         else:
-            self.name = 'no ID'
-        
+            self._name = 'no ID'
+
         U01, U02, U03, U04, U05, U06, U07, U08, U09, U10, U11, U12, U13 =[
             np.empty((len(time),len(yy), len(xx))) for i in range(13)]
-        
+
         for j in range(len(self.time)):
             U01[j,:,:] = file_list[j]['U01'][:,:]
             U02[j,:,:] = file_list[j]['U02'][:,:]
@@ -212,17 +214,85 @@ class hifi_class(object):
             U11[j,:,:] = file_list[j]['U11'][:,:]
             U12[j,:,:] = file_list[j]['U12'][:,:]
             U13[j,:,:] = file_list[j]['U13'][:,:]
-        
-        self.ni = U01
-        self.Az = -U02
-        self.Bz = U03
-        self.Vix = np.divide(U04, U01)
-        self.Viy = np.divide(U05, U01)
-        self.Viz = np.divide(U06, U01)
-        self.Jz = U07
-        self.pi = U08
-        self.nn = U09
-        self.Vnx = np.divide(U10, U09)
-        self.Vny = np.divide(U11, U09)
-        self.Vnz = np.divide(U12, U09)
-        self.pn = U13
+
+        self._ni = U01
+        self._Az = -U02
+        self._Bz = U03
+        self._Vix = np.divide(U04, U01)
+        self._Viy = np.divide(U05, U01)
+        self._Viz = np.divide(U06, U01)
+        self._Jz = U07
+        self._pi = U08
+        self._nn = U09
+        self._Vnx = np.divide(U10, U09)
+        self._Vny = np.divide(U11, U09)
+        self._Vnz = np.divide(U12, U09)
+        self._pn = U13
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def file_list(self):
+        return self._file_list
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def time(self):
+        return self.time
+
+    @property
+    def ni(self):
+        return self._ni
+
+    @property
+    def Az(self):
+        return self._Az
+
+    @property
+    def Bz(self):
+        return self._Bz
+
+    @property
+    def Viy(self):
+        return self._Viy
+
+    @property
+    def Viz(self):
+        return self._Viz
+
+    @property
+    def Jz(self):
+        return self._Jz
+
+    @property
+    def pi(self):
+        return self._pi
+
+    @property
+    def nn(self):
+        return self._nn
+
+    @property
+    def Vnx(self):
+        return self._Vnx
+
+    @property
+    def Vny(self):
+        return self._Vny
+
+    @property
+    def Vnz(self):
+        return self._Vnz
+
+    @property
+    def pn(self):
+        return self._pn
