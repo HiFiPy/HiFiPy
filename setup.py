67ba99f076c70f61e8a39ed54fe91f91e402660f
Nick Murphy <namurphy@cfa.harvard.edu>
2019-05-07 18:53:46 -0400

    Allow running `python setup.py test` to run tests

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-from __future__ import print_function
-from os.path import expanduser,isdir, isfile
 from setuptools import setup
 
-
-#import h5py
-#import numpy as np
-import glob
 
 setup(name='HiFiPy',
 	version='0.1',
 	description='Python software for analysis of HiFi simulations.',
 	url='https://github.com/HiFiPy/HiFiPy',
-	author='Nicholas A. Murphy',
+	author='Nicholas A. Murphy and Eric S. Mukherjee',
 	author_email='namurphy@cfa.harvard.edu',
 	license='BSD-3-clause',
 	packages=['hifipy','hifipy.io'],
 	install_requires=['numpy','h5py'],
-	include_package_data=True
-	)
+	include_package_data=True,
+    setup_requires = ["pytest-runner", "h5py", "numpy"],
+    tests_require = ["pytest", "h5py", "numpy"],
+)
