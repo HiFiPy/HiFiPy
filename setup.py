2f3e703b8ecc21d79588afe315fbcd5e6360ef67
Nick Murphy <namurphy@cfa.harvard.edu>
2019-05-09 20:40:27 -0400

    Add SciPy to list of requirements and fix tabs

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
 
-setup(name='HiFiPy',
+setup(
+	name='HiFiPy',
 	version='0.1',
 	description='Python software for analysis of HiFi simulations.',
 	url='https://github.com/HiFiPy/HiFiPy',
@@ -9,8 +10,8 @@ setup(name='HiFiPy',
 	author_email='namurphy@cfa.harvard.edu',
 	license='BSD-3-clause',
 	packages=['hifipy','hifipy.io'],
-	install_requires=['numpy','h5py'],
+	install_requires=['numpy', 'scipy', 'h5py'],
 	include_package_data=True,
-    setup_requires = ["pytest-runner", "h5py", "numpy"],
-    tests_require = ["pytest", "h5py", "numpy"],
+	setup_requires = ["pytest-runner", "h5py", "numpy"],
+	tests_require = ["pytest", "h5py", "numpy"],
 )
